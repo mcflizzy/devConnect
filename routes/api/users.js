2be5ee9e1265f5225a7289a79454975ca3d5fc15
Scott Guidry <scottpguidry@gmail.com>
2021-07-19 21:59:50 -0500

    added validation for name, email and password with express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,27 @@
-import express from 'express';
+import   express                   from 'express';
+import { check, validationResult } from 'express-validator';
+
+// initialize router
 const router = express.Router();
 
 // @router  POST api/users 
 // @desc    Register user
 // @access  Public
-export const usersRoute = router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('--- User route ---')
-});
+export const usersRoute = 
+    router.post(
+        '/',
+        [
+            check('name', 'Name is required').not().isEmpty(),
+            check('email', 'Please include a valid email').isEmail(),
+            check('password', 'PW must be 6 or more characters').isLength({min: 6})
+        ],
+        (req, res) => {
+                        const errors = validationResult(req);
+                        
+                        if(!errors.isEmpty()){
+                            return res.status(400).json({errors: errors.array()});
+                        }
+
+                        console.log(req.body);
+                        res.send('--- User route ---')
+                    });
